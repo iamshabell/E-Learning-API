742fc98fc879ce316b88e1ade73b22ede847edfd
iamshabell <91321698+iamshabell@users.noreply.github.com>
2022-03-30 08:30:46 +0300

    feature: implement NESTJS API and core library

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,16 +5,20 @@
 
 import { Logger } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
+import { ConfigService } from '@nestjs/config'
 
 import { AppModule } from './app/app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  const config = app.get(ConfigService)
   const globalPrefix = 'api'
   app.setGlobalPrefix(globalPrefix)
   const port = process.env.PORT || 3333
   await app.listen(port)
   Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`)
+  Logger.log(`Running in ${config.get('NODE_ENV')} mode`)
+  
 }
 
 bootstrap()
